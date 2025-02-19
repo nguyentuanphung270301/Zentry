9aa7a4caca14e755cf0bea57b665a6363cc55f5a
nguyentuanphung270301 <nguyentuanphung270301@gmail.com>
2025-02-19 10:30:17 +0700

    Mô tả thay đổi

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -35,7 +35,7 @@ const About = () => {
             </div>
             <div className="h-dvh w-screen" id="clip">
                 <div className="mask-clip-path about-image">
-                    <img src="/public/img/about.webp" alt="Background"  className="absolute left-0 top-0 size-full object-cover"/>
+                    <img src="img/about.webp" alt="Background"  className="absolute left-0 top-0 size-full object-cover"/>
                 </div>
             </div>
         </div>
