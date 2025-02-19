be82a95ebd11d156b578fb7e8c47e7337040fe00
nguyentuanphung270301 <nguyentuanphung270301@gmail.com>
2025-02-19 10:35:08 +0700

    Mô tả thay đổi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -62,7 +62,7 @@ const Navbar = () => {
             <header className="absolute top-1/2 w-full -translate-y-1/2">
                 <nav className="flex size-full items-center justify-between p-4">
                     <div className="flex items-center gap-7">
-                        <img src="/public/img/logo.png" alt="logo" className="w-10" />
+                        <img src="img/logo.png" alt="logo" className="w-10" />
                         <Button id='product-button' title="Products" rightIcon={<TiLocationArrow />} containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1" />
                     </div>
                     <div className="flex h-full items-center">
